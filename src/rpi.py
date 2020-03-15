2fd09d1ba10a142e6325722a137d4dfa5b41d7d1
Tom <tomhosking@gmail.com>
2020-03-15 13:18:01 +0000

    Update rpi

--- src/rpi.py
+++ src/rpi.py
@@ -60,7 +60,7 @@ try:
             err_col = (255,0,0)
             err_width = min(width-2, num_errs)
             for x in range(err_width):
-                unicorn.set_pixel(x,2,*err_col)
+                unicorn.set_pixel(x,3,*err_col)
 
             unicorn.set_pixel(width-1, height-1, 100,0,100)
 
