96a2f74d2d33bffda6d5fafe5a3089f666f475ea
Tom <tomhosking@gmail.com>
2020-03-15 10:18:31 +0000

    Testing servers

--- src/proxy.py
+++ src/proxy.py
@@ -14,3 +14,47 @@ app = Flask(__name__)
 def home():
     
     return "Mckenzie Proxy is running"
+
+@app.route('/api/update')
+def update():
+    
+    try:
+        with TinyDB('./db/db.json', storage=CachingMiddleware(JSONStorage)) as db:
+            table = db.table('status')
+
+            stat_objs = table.search(Query().type == 'status')
+            if len(stat_objs) < 1:
+                table.insert({'type': 'status'})
+
+            new_status = {
+                'updated': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            }
+
+            table.update(**new_status, Query().type == 'status')
+
+    except Exception as e:
+        return str(e)
+
+    finally:
+        return "OK"
+
+
+
+
+@app.route('/api/get')
+def get():
+    try:
+        with TinyDB('./db/db.json', storage=CachingMiddleware(JSONStorage)) as db:
+            table = db.table('status')
+
+            stat_objs = table.search(Query().type == 'status')
+
+            if len(stat_objs) < 1:
+                table.insert({'type': 'status'})
+
+            stat_obj = table.search(Query().type == 'status')[0]
+
+            return json.dumps(stat_obj)
+
+    except Exception as e:
+        return str(e)
