8dc7833c99dfbf3f2142b38c65eb747e3e7f0f9a
Tom <tomhosking@gmail.com>
2020-03-24 15:32:17 +0000

    Update hook to include partition as key

--- scripts/hook.sh
+++ scripts/hook.sh
@@ -31,13 +31,13 @@ done
 
 if [ $newjob -eq 1 ]
 then
-    curl -s --data "jobid=$SLURM_JOB_ID&status=submitted" http://${MCKENZIE_ENDPOINT}/hooks/update_job/
+    curl -s --data "jobid=$SLURM_JOB_ID&partition=$SLURM_JOB_PARTITION&status=submitted" http://${MCKENZIE_ENDPOINT}/hooks/update_job/
 elif [ -v status ]
 then
-    curl -s --data "jobid=$SLURM_JOB_ID&status=$status&hostname=$SLURM_JOB_NODELIST&partition=$SLURM_JOB_PARTITION" http://${MCKENZIE_ENDPOINT}/hooks/update_job/
+    curl -s --data "jobid=$SLURM_JOB_ID&partition=$SLURM_JOB_PARTITION&status=$status&hostname=$SLURM_JOB_NODELIST" http://${MCKENZIE_ENDPOINT}/hooks/update_job/
 elif [ -v jobname ]
 then
-    curl -s --data "jobid=$SLURM_JOB_ID&jobname=$jobname" http://${MCKENZIE_ENDPOINT}/hooks/update_job/
+    curl -s --data "jobid=$SLURM_JOB_ID&partition=$SLURM_JOB_PARTITION&jobname=$jobname" http://${MCKENZIE_ENDPOINT}/hooks/update_job/
 fi
 
 if [ "$configfile" != "" ]
